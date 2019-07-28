91a5a0a049c6be55ea73db84c8184424ffa420fe
Makeen Sabree <makeen.sabree@gmail.com>
2019-07-27 20:31:21 -0400

    Generate random four digit int for sync.

--- routes/alexa.js
+++ routes/alexa.js
@@ -3,19 +3,47 @@ var router = express.Router();
 
 const dbConnect = require('../utils/dbConnect');
 
+// Generate codes for a user
 router.get('/verification-codes', function(req, res, next) {
     dbConnect()
     .then((connection) => {
-        console.log('Hello World')
+        
+        const randomFourDigitInt = Math.floor(1000 + Math.random() * 9000);
+        const randomColor = colors[Math.floor(Math.random() * colors.length)];
+
         res.send({
-            codeOne: 'red',
-            codeTwo: '15'
+            codeOne: randomColor,
+            codeTwo: randomFourDigitInt
         });
     })
-    .catch(() => {
+    .catch((err) => {
         res.send({
-            codeOne: 'red',
-            codeTwo: '15'
+            message: 'oops',
+            err,
+        });
+    })
+});
+
+// Alexa endpoint to sync device with app profile
+router.post('/sync-profile', function(req, res, next) {
+
+    const { codeOne, codeTwo, alexaMetaData } = req.body;
+
+    dbConnect()
+    .then((connection) => {
+        
+        console.log(codeOne, codeTwo)
+
+        // once we have alexa sending codes we'd need to find the user's profile who initiated the sync
+
+        res.send({
+            message: 'ok'
+        });
+    })
+    .catch((err) => {
+        res.send({
+            message: 'oops',
+            err,
         });
     })
 });
