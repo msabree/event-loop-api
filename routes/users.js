437acf6614935fbd38cfff47a78c647dc81d720e
Makeen Sabree <makeen.sabree@gmail.com>
2019-08-14 19:29:50 -0400

    S3 profile picture upload.

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,11 @@
 var express = require('express');
 var router = express.Router();
+const get = require('lodash/get');
 const uuidv4 = require('uuid/v4');
+const S3 = require('aws-sdk/clients/s3');
+
+// AWS S3
+const s3Bucket = new S3({ params: { Bucket: 'flaker-images' } });
 
 // TWILIO
 const accountSid = process.env.TWILIO_ACCOUNT_SID;
@@ -10,6 +15,31 @@ const client = require('twilio')(accountSid, authToken);
 const dbConnect = require('../utils/dbConnect');
 const getSession = require('../utils/getSession');
 const USERS_TABLE = 'users';
+
+const uploadPhotoToS3 = (updateFields, sessionToken) => {
+    return new Promise((resolve, reject) => {
+        const base64ProfilePic = get(updateFields, 'profilePic', null);
+        if (base64ProfilePic !== null) {
+            const buf = new Buffer(base64ProfilePic.replace(/^data:image\/\w+;base64,/, ''), 'base64');
+            const base64Photo = {
+                Key: sessionToken,
+                Body: buf,
+                ContentEncoding: 'base64',
+                ContentType: 'image/jpeg',
+            };
+            s3Bucket.putObject(base64Photo, (err) => {
+                if (err) {
+                    reject(err);
+                } else {
+                    updateFields.profilePic = `https://s3.amazonaws.com/aliaspayio-images/${sessionToken}?timestamp=${new Date().getTime()}`;
+                    resolve(updateFields);
+                }
+            });
+        } else {
+            resolve(updateFields);
+        }
+    });
+}
 
 router.get('/:sessionToken', function(req, res, next) {
 
@@ -25,15 +55,18 @@ router.get('/:sessionToken', function(req, res, next) {
         return STORE.connection.collection(USERS_TABLE).find({ userId }).toArray();
     })
     .then((arrProfiles) => {
+        // EXACT COPY IN PUT
         let profile = {};
         if(arrProfiles.length === 1){
             const {
                 username,
+                displayName,
                 profilePic,
             } = arrProfiles[0];
 
             profile = {
                 username,
+                displayName,
                 profilePic
             }
         }
@@ -42,6 +75,50 @@ router.get('/:sessionToken', function(req, res, next) {
             profile,
             message: ''
         })
+        // EXACT COPY IN PUT
+    })
+    .catch((err) => {
+        res.send({
+            success: false,
+            message: err.message || err
+        })  
+    })
+});
+
+router.put('/:sessionToken', function(req, res, next) {
+
+    const { sessionToken } = req.params;
+    const STORE = {};
+
+    dbConnect.then((connection) => {
+        STORE.connection = connection;
+        return getSession(sessionToken, connection);
+    })
+    .then(() => uploadPhotoToS3(req.body, sessionToken))
+    .then((update) => STORE.connection.collection(USERS_TABLE).updateOne({ sessionToken }, { $set: update }))
+    .then(() => STORE.connection.collection(USERS_TABLE).find({ sessionToken }).toArray())
+    .then((arrProfiles) => {
+        // EXACT COPY IN GET
+        let profile = {};
+        if(arrProfiles.length === 1){
+            const {
+                username,
+                displayName,
+                profilePic,
+            } = arrProfiles[0];
+
+            profile = {
+                username,
+                displayName,
+                profilePic
+            }
+        }
+        res.send({
+            success: true,
+            profile,
+            message: ''
+        })
+        // EXACT COPY IN GET
     })
     .catch((err) => {
         res.send({
@@ -89,7 +166,7 @@ router.get('/verification/:phoneNumber/:code', function(req, res, next) {
                     phoneNumber,
                     profilePic: 'https://flaker-images.s3.amazonaws.com/default-profile.png',
                     joined: new Date().toISOString(),
-                    legalName: '', // optional
+                    displayName: '', // optional
                     email: '', // optional
                     username: 'user_' + new Date().getTime(), // pregenerated, can be changed later
                     pushObject: {}, // the id for push notifications, this id rotates so we have a route to update it
