7e0a0c8da223e5ef9ae509d1eb60c3720015c28e
Makeen Sabree <makeen.sabree@gmail.com>
2019-09-22 14:06:46 -0400

    Attempt to send push notification on android.

--- utils/pushNotification.js
+++ utils/pushNotification.js
@@ -1,5 +1,13 @@
+const admin = require('firebase-admin');
 const uuidv4 = require('uuid/v4');
 const appConstants = require('./constants');
+const get = require('lodash/get');
+
+// Push notifications configuration for Android
+admin.initializeApp({
+    credential: admin.credential.cert(process.env.SERVICE_ACCOUNT_JSON),
+    databaseURL: 'https://flaker-8a057.firebaseio.com',
+});
 
 /**
  * Helper to send push notifications and update notifications collection.
@@ -9,19 +17,49 @@ const appConstants = require('./constants');
  * @param {Object} message - The notification message to send.
 */
 module.exports = function(connection, userId, type, message){
-    console.log(userId, type, message)
     return new Promise((resolve) => {
-        // const STORE = {};
-        connection.collection(appConstants.NOTIFICATIONS_TABLE).insertOne({
-            notificationId: uuidv4(),
-            userId,
-            type,
-            message,
-            createdDatetime: new Date().toISOString(),
-            read: false,
+        const STORE = {};
+        connection.collection(appConstants.USERS_TABLE).find({userId}).toArray()
+        .then((arrUsers) => {
+            if(arrUsers.length !== 0){
+                throw new Error('single user not found')
+            }
+            else{
+                STORE.userObj = arrUsers[0];
+            }
+
+            return connection.collection(appConstants.NOTIFICATIONS_TABLE).insertOne({
+                notificationId: uuidv4(),
+                userId,
+                type,
+                message,
+                createdDatetime: new Date().toISOString(),
+                read: false,
+            })
         })
         .then(() => {
-            // send push!
+            const androidPush = {
+                notification: {
+                    title: 'Event Loop Notification',
+                    body: `${message}`,
+                },
+                data: {
+                    datetime: new Date().toISOString()
+                },
+            };
+
+            if (get(STORE.userObj, 'pushObject.os', '').toLowerCase() === 'android') {
+                admin.messaging().sendToDevice(get(STORE.userObj, 'pushObject.token', ''), androidPush)
+                .then(() => {
+                    resolve();
+                })
+                .catch((e) => {
+                    throw new Error(e);
+                })
+            }
+
+            console.log(androidPush)
+
             resolve();
         })
         .catch((err) => {
